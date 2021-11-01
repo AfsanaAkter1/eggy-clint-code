3d9ea2877e9b7ba53c5d87fb9813f2e18d059ae2
tania <you@example.com>
2021-11-01 23:24:28 +0600

    forth commim

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,7 +3,7 @@ import "./Banner.css"
 
 const Banner = () => {
     return (
-        <div>
+        <div className="main">
             <div className="banner">
                 <div className="banner-card">
                     <h1 className="text-warning">HUNGRY?</h1>
